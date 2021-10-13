b019fe11854f261f5ebf10d67a33ca8ac4021035
Wisnu <wisnuaryadipa93@gmail.com>
2021-10-13 15:34:10 +0700

    add default architecture expressjs-me

---
+++ src/config/sequelize.ts
@@ -0,0 +1,30 @@
+import config from '../config'
+import {IDBEnv, IDBType} from '../interfaces/IConfigs'
+
+const env = (process.env.NODE_ENV || 'development');
+const dbConfig: IDBType = config.db[env];
+
+const sequelize : any = {
+    "host": dbConfig.postgres?.host,
+    "username": dbConfig.postgres?.username || "",
+    "password": dbConfig.postgres?.password,
+    "database": dbConfig.postgres?.database || "",
+    "schema": dbConfig.postgres?.schema || "",
+    "dialect": "postgres",
+    "logging": true,
+    "timezone": "+07:00",
+    "operatorsAliases": false,
+    "define": {
+        "freezeTableName": true
+    },
+    "query": { "raw": true },
+    "pool": {
+      "max": 5,
+      "min": 0,
+      "acquire": 20000,
+      "idle": 5000,
+      "evict": 1000
+    }
+}
+
+export default sequelize;
---
+++ src/loaders/Logger.ts
@@ -0,0 +1,21 @@
+import winston from 'winston';
+import config from '../config';
+import {transports} from '../config/logging'
+
+const LoggerInstance = winston.createLogger({
+  level: config.logs.level,
+  levels: winston.config.npm.levels,
+  format: winston.format.combine(
+    winston.format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss'
+    }),
+    winston.format.colorize({ colors: { info: 'blue' } }),
+    winston.format.label({label: "dusinbox"}),
+    winston.format.errors({ stack: true }),
+    winston.format.splat(),
+    winston.format.json()
+  ),
+  transports: transports
+});
+
+export default LoggerInstance;

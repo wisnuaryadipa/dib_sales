3c372793f1d24842d4426b715f9f8c59e1875adb
Wisnu <wisnuaryadipa93@gmail.com>
2021-10-14 07:05:47 +0700

    fix ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     apps : [{
       name   : "api-sales",
-      script : ".src/app.js",
-      node_args: "-r dotenv/config -r ./register-paths.js",
+      script : "./src/app.js",
+      node_args: "-r tsconfig-paths/register",
       merge_logs: true,
       max_restarts: 20,
       instances: 4,

ee077b16021283469590796a34d51bdf32c07672
Wisnu <wisnuaryadipa93@gmail.com>
2021-10-14 09:20:17 +0700

    fix ecosystem config

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,15 +1,14 @@
+export {}
 import expressLoader from '@src/loaders/express';
 import express from 'express';
 import sequelizeLoader from '@src/loaders/sequelize';
-import Logger from '@src/loaders/logger';
-
-
+// import Logger from '@src/loaders/Logger';
 
 const loaders = async ({ expressApp } : {expressApp: express.Application}) => {
 
     const sequelizeConnection = await sequelizeLoader().authenticate();
     sequelizeConnection;
-    Logger.info('✌️ DB loaded and connected!');
+    // Logger.info('✌️ DB loaded and connected!');
     
     await expressLoader(expressApp)
 

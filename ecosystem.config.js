51347689345aa9676698094f8b28a063d41a0ccd
Wisnu <wisnuaryadipa93@gmail.com>
2021-10-15 15:10:51 +0700

    edit ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps : [{
       name   : "api-sales",
       script : "./src/app.ts",
-      node_args: "-r tsconfig-paths/register",
+      node_args: ["-r tsconfig-paths/register"],
       merge_logs: true,
       max_restarts: 20,
     }]
